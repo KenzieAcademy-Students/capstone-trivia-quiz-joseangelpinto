d3c9a15b4dc4181450c1198baa074c299f20a860
kenzieadmin <kenzieadmin@users.noreply.github.com>
2021-08-06 00:57:11 +0000

    deploy: 76c906ce23dccf53cb98bf15c317ae0aed31047a

--- code.js
+++ code.js
@@ -1,13 +1,32 @@
-// To run this assignment, right click on index.html in the Visual Studio Code file explorer to the left
-// and select "Open with Live Server"
+// To run this assignment, right click on index.html in the Visual Studio Code file explorer to the left 
+// and select "Open with Live Server" 
 
-// YOUR CODE HERE!
-function getQuestion() {
-    fetch("https://jservice.io/api/random")
-    .then(res=> res.json())
-    .then(res=> console.log(res))
 
-  
-}
-getQuestion()
-// buscar 100 preguntas
+// YOUR CODE HERE! 
+// To run this assignment, right click on index.html in the Visual Studio Code file explorer to the left 
+// and select "Open with Live Server" 
+// YOUR CODE HERE! 
+let display = document.querySelector("#display") 
+display.innerHTML = "<button id='Start'>Start Game</button>" 
+document.querySelector("#Start").addEventListener("click", displayQ) 
+async function displayQ() { 
+    let question = await getQuestion() 
+    console.log(question) 
+    display.innerHTML = `<h3>The category is: ${question[0].category.title}</h3> 
+    <p>Use the clue to answers the trivia:</p> 
+    <p>${question[0].question}</p> 
+    <input></input>` 
+}  
+
+function getQuestion() { 
+    return fetch("https://jservice.io/api/random") 
+    .then(res=> res.json()) 
+} 
+
+getQuestion() 
+
+// buscar 100 preguntas 
+
+ 
+
+ 
