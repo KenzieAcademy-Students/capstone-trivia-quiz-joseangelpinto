34ff7620bff4f0828a3b8e7eee839fadf91a19c8
kenzieadmin <kenzieadmin@users.noreply.github.com>
2021-08-20 00:04:59 +0000

    deploy: a1cf8583ad38921a841d3542139a39930d1d6288

--- code.js
+++ code.js
@@ -7,26 +7,28 @@ display.innerHTML = "<button id='Start'>Start Game</button>"
 document.querySelector("#Start").addEventListener("click", displayQ)
 
  async function displayQ() { 
-    let question = await getQuestion()  
+    let question = await getQuestion() 
+    let randomQ = question.clues[Math.floor(Math.random() * question.clues.length)] 
     console.log(question)
-    display.innerHTML = `<h3>The category is: ${question[0].category.title}</h3>
-    <p>Use the clue to answers the trivia:</p> 
-    <p>${question[0].question}</p> 
+    console.log(randomQ) 
+    display.innerHTML = `<h3>The category is: ${question.title}</h3>
+    <p>Use the clue to answers the trivia:</p>
+    <p>${randomQ.question}</p> 
     <input></input>
     <button id="Answer">Answer Question</button>` 
     let input = document.querySelector("input")
     document.querySelector("#Answer").addEventListener("click", checkAnswer) 
     function checkAnswer(){
-        if (input.value.toLowerCase()===question[0].answer.toLowerCase()){
+        if (input.value.toLowerCase()===randomQ.answer.toLowerCase()){
         console.log("you are correct")
         score += 1
         display.innerHTML+=`<p>You are Correct</p>
-        <p>The Correct Answer is: ${question[0].answer}</p><p>Your Score:${score}</p>`
+        <p>The Correct Answer is: ${randomQ.answer}</p><p>Your Score:${score}</p>`
         }else{
         console.log("you are wrong")
         score = 0
         display.innerHTML+=`<p>You are Wrong</p>
-        <p>The Correct Answer is: ${question[0].answer}</p>`
+        <p>The Correct Answer is: ${randomQ.answer}</p>`
         }
         display.innerHTML+="<button id= 'playAgain'>Play Again</button>"
         document.querySelector("#Answer").removeEventListener("click", checkAnswer) 
@@ -42,6 +44,11 @@ function answerQ(event) {
  function getQuestion() { 
     return fetch("https://jservice.io/api/random") 
     .then(res=> res.json()) 
+    .then(data=> {
+        console.log(data)
+        return fetch(`https://jservice.io/api/category?id=${data[0].category.id}`)
+        .then(res=> res.json())
+    })
 } 
 
 
